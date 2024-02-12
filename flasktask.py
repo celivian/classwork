2d009b13a7e49fb85b57eba352e5c76a549f939c
shergeu <serzh.ilyukin@vk.ru>
2024-02-12 17:07:25 +0300

    задача 3

--- flasktask.py
+++ flasktask.py
@@ -13,6 +13,14 @@ def train(prof):
     return render_template('training.html', prof=prof)
 
 
+@app.route('/list_prof/<flag>')
+def prof(flag):
+    proflist = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                'инженер по терраформированию', 'климатолог',
+                'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                'штурман', 'пилот дронов']
+    return render_template('proflist.html', flag=flag, proflist=proflist)
 
 
 if __name__ == '__main__':
