89efb689b60a7d710cc8dc48294bcf05b1ad6f7d
shergeu <serzh.ilyukin@vk.ru>
2024-02-12 16:42:31 +0300

    задача 2

--- flasktask.py
+++ flasktask.py
@@ -8,5 +8,12 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def train(prof):
+    return render_template('training.html', prof=prof)
+
+
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
