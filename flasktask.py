8217acd8431f8d96349e2eb24a4352fbde15e295
shergeu <serzh.ilyukin@bk.ru>
2024-03-17 12:14:30 +0300

    hoomework otbor

--- flasktask.py
+++ flasktask.py
@@ -64,5 +64,16 @@ def success():
     return render_template('success.html')
 
 
+@app.route('/distribution')
+def distribution():
+    return render_template('distribution.html', user_list=[
+        'Ридли Скотт',
+        'Энди Уир',
+        'Марк Уотни',
+        'Венката Капур',
+        'Тедди Сандерс',
+        'Шон Бин'])
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
